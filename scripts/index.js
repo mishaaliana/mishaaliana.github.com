25468dbf3d43dcad2ae8886cce96f4cbf59fa274
Alyssa Gibson <lissy0808@yahoo.com>
2012-02-01 20:00:22 -0600

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+﻿//Makes #nav links appear at 50% opacity until hover event
+
+$(document).ready(function () {
+//on load #nav links appear at 50% opacity
+    $('#nav a').animate({
+        opacity: .5
+    });
+    $("#nav a").hover(function () {
+    //when moused over opacity becomes 100%
+        $(this).stop().animate({ "opacity": "1" }, "slow");
+    }, function () {
+    //when moused off opacity reverts to 50%
+        $(this).stop().animate({ "opacity": ".5" }, "slow");
+    });
+
+ });
+
